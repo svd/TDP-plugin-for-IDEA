c0e17c95eedf00bbb8627e1d8a5a521af088785d
Mikalai_Churakou <Lanyon123>
2016-04-20 15:55:06 +0300

    - fix for one module case

--- src/com/tdp/decorator/ModuleDecorator.java
+++ src/com/tdp/decorator/ModuleDecorator.java
@@ -1,30 +1,23 @@
-package com.tdp.decorator;
+ package com.tdp.decorator;
 
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.ProjectViewNode;
 import com.intellij.ide.projectView.ProjectViewNodeDecorator;
 import com.intellij.ide.util.treeView.PresentableNodeDescriptor;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.roots.ContentIterator;
-import com.intellij.openapi.roots.ModuleFileIndex;
-import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.packageDependencies.ui.PackageDependenciesNode;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.SimpleTextAttributes;
-import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.awt.Color;
-import java.awt.Font;
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
 
-/**
+ /**
  * Created by Mikalai_Churakou on 4/19/2016.
  */
 public class ModuleDecorator implements ProjectViewNodeDecorator {
@@ -39,45 +32,27 @@ public class ModuleDecorator implements ProjectViewNodeDecorator {
     @Override
     public void decorate(ProjectViewNode node, PresentationData data) {
 
-        ModuleManager mm = ModuleManager.getInstance(node.getProject());
-        for (Module module : mm.getModules()){
-
-            /* decorate only module node */
-            if (module.getName().equals(node.getName())){
-
-                if (!descriptionCache.containsKey(module.getName())){
-                    ModuleFileIndex moduleIndex = ModuleRootManager.getInstance(module).getFileIndex();
-
-                    final String[] description = new String[1];
-
-                    moduleIndex.iterateContent(new ContentIterator(){
+            VirtualFile nodeVirtualFile = node.getVirtualFile();
+            String fileName = nodeVirtualFile.getName();
+            if (!descriptionCache.containsKey(fileName)) {
 
 
-                        @Override
-                        public boolean processFile(VirtualFile fileOrDir) {
+                VirtualFile f = nodeVirtualFile.findChild(PROJECT);
+                if (f != null) {
+                    String description = extractDescription(f);
+                    descriptionCache.put(fileName, description);
 
-                            VirtualFile f = fileOrDir.findChild(PROJECT);
-
-                            if (f != null){
-                                description[0] = extractDescription(f);
-
-                            }
-
-                            return false;
-                        }
-                    });
-
-                    descriptionCache.put(module.getName(), description[0]);
-
+                    System.out.println(fileName + " * " + description);
                 }
 
-                addModuleDescription(data, descriptionCache.get(module.getName()));
-
-
-                break;
 
             }
-        }
+
+            addModuleDescription(data, descriptionCache.get(fileName));
+
+
+
+
 
     }
 
@@ -106,10 +81,20 @@ public class ModuleDecorator implements ProjectViewNodeDecorator {
     }
 
     private void addModuleDescription(PresentationData data, String description) {
-        if (description!= null){
-            SimpleTextAttributes attr = new SimpleTextAttributes(Font.PLAIN, Color.black);
-            PresentableNodeDescriptor.ColoredFragment fragment = new PresentableNodeDescriptor.ColoredFragment(description, attr);
-            data.getColoredText().add(fragment);
+        if (description != null){
+            String text = " [" + description + "]";
+            List<PresentableNodeDescriptor.ColoredFragment> coloredFragmentList = data.getColoredText();
+
+            /*decorate module*/
+            if (coloredFragmentList.size() > 0){
+                SimpleTextAttributes attr = new SimpleTextAttributes(Font.PLAIN, Color.black);
+                PresentableNodeDescriptor.ColoredFragment fragment = new PresentableNodeDescriptor.ColoredFragment(text, attr);
+                coloredFragmentList.add(fragment);
+            } else { //decorate node
+                data.setPresentableText(data.getPresentableText() + text);
+            }
+
+
         }
 
 
