eae3f1afa87421b8890f5cc86cf878208d0b75db
Siarhei Nahel <siarhei.nahel@datalex.com>
2016-11-03 17:14:57 +0300

    added progress indicator

--- src/com/tdp/workspace/generator/Controller.java
+++ src/com/tdp/workspace/generator/Controller.java
@@ -1,6 +1,8 @@
 package com.tdp.workspace.generator;
 
 import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ex.ApplicationManagerEx;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.impl.ProjectImpl;
 import com.intellij.openapi.roots.*;
 import com.intellij.openapi.roots.impl.libraries.LibraryEx;
@@ -11,7 +13,6 @@ import com.tdp.workspace.generator.fileutils.TdpPluginPropertiesReader;
 import com.tdp.workspace.generator.utils.GeneratorModuleDep;
 import com.tdp.workspace.generator.utils.GeneratorSourceContent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.impl.ModuleManagerImpl;
@@ -54,95 +55,113 @@ public class Controller {
         ModuleManager manager = ModuleManagerImpl.getInstance(project);
         LibraryTable libraryTable = LibraryTablesRegistrar.getInstance().getLibraryTable(project);
         Library[] libraries = libraryTable.getLibraries();
-        for (Library library : libraries) {
-            CommandProcessor.getInstance().executeCommand(project, new Runnable() {
-                public void run() {
-                    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+        ApplicationManagerEx.getApplicationEx().runProcessWithProgressSynchronously(new Runnable() {
+            @Override
+            public void run() {
+                for (Library library : libraries) {
+                    ProgressManager.getInstance().getProgressIndicator().setText("Remove library: " + library.getName());
+                    doWriteAction(new Runnable() {
+                        @Override
                         public void run() {
                             libraryTable.removeLibrary(library);
                         }
                     });
                 }
-            }, null, null);
-        }
+            }
+        }, "Removing libraries", false, project);
 
         GeneratorModuleDep generatorModuleDep = new GeneratorModuleDep(path, baseModules);
         allDepend = generatorModuleDep.getModuleNames();
         ModulesDepUtil modulesDepUtil = ModulesDepUtil.getInstance(path);
         final List<Module> modules = Arrays.asList(manager.getModules());
-        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+        ApplicationManagerEx.getApplicationEx().runProcessWithProgressSynchronously(new Runnable() {
+            @Override
             public void run() {
-                for (Module module : modules){
+                for (Module module : modules) {
                     String moduleName = module.getName();
+                    ProgressManager.getInstance().getProgressIndicator().setText("Remove module: " + moduleName);
                     if (!allDepend.contains(moduleName)){
-                        manager.disposeModule(module);
+                        doWriteAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                manager.disposeModule(module);
+                            }
+                        });
                     }
                 }
             }
-        });
+        }, "Removing modules", false, project);
+
         Map<String, String> artifactory = modulesDepUtil.getArtifactoryModules();
-        for (String moduleName : allDepend) {
-            String pathToModule = MessageFormat.format(Constants.PATTERN_PATH_TO_MODULE_FILE, path, moduleName);
-            doWriteAction(new Runnable() {
-                @Override
-                public void run() {
-                    Module module = manager.newModule(pathToModule, JAVA_MODULE);
-                    String rootContent = path + Constants.SLASH + moduleName;
-                    String output = rootContent + Constants.CLASSES_DIR;
-                    ModuleRootModificationUtil.updateModel(module, new Consumer<ModifiableRootModel>() {
+        ApplicationManagerEx.getApplicationEx().runProcessWithProgressSynchronously(new Runnable() {
+            @Override
+            public void run() {
+                allDepend.add("dlex_build_templates");
+                for (String moduleName : allDepend) {
+                    ProgressManager.getInstance().getProgressIndicator().setText("Load module: " + moduleName);
+                    String pathToModule = MessageFormat.format(Constants.PATTERN_PATH_TO_MODULE_FILE, path, moduleName);
+                    doWriteAction(new Runnable() {
                         @Override
-                        public void consume(ModifiableRootModel model) {
-                            CompilerModuleExtension extension = model.getModuleExtension(CompilerModuleExtension.class);
-                            ContentEntry rootContentEntry = model.addContentEntry(VfsUtilCore.pathToUrl(rootContent));
-                            extension.inheritCompilerOutputPath(false);
-                            extension.setCompilerOutputPath(VfsUtilCore.pathToUrl(output));
-                            extension.setCompilerOutputPathForTests(VfsUtilCore.pathToUrl(output));
-                            extension.setExcludeOutput(true);
-                            model.inheritSdk();
-                            boolean hasModuleInArtifactory = artifactory.containsKey(moduleName);
-                            OrderEntry[] orderEntries = model.getOrderEntries();
-                            NavigableSet<String> modulesDep = modulesDepUtil.getDepsForModule(moduleName);
-                            for (OrderEntry entry : orderEntries) {
-                                if ((entry instanceof ModuleOrderEntry) || (entry instanceof LibraryOrderEntry)) {
-                                    model.removeOrderEntry(entry);
-                                }
-                            }
-                            for (String moduleDep : modulesDep) {
-                                model.addInvalidModuleEntry(moduleDep).setExported(true);
-                            }
-                            String pathToLib;
-                            if (hasModuleInArtifactory && artifactoryDep) {
-                                pathToLib = artifactory.get(moduleName);
-                            } else {
-                                try {
-                                    GeneratorSourceContent.generateRes(path, rootContentEntry, rootContent);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                                pathToLib = path + Constants.SLASH + moduleName + Constants.SLASH + Constants.LIB_DIR;
-                            }
-                            File libs = new File(pathToLib);
-                            if(libs.exists()) {
-                                LibraryImpl library = (LibraryImpl) model.getModuleLibraryTable().createLibrary("Library");
-                                LibraryEx.ModifiableModelEx libraryModel = library.getModifiableModel();
-                                VirtualFile file = VfsUtilCore.findRelativeFile(pathToLib, project.getBaseDir());
-                                libraryModel.addJarDirectory(file, true);
-                                LibraryOrderEntry entry = model.findLibraryOrderEntry(library);
-                                assert entry != null : library;
-                                entry.setScope(DependencyScope.COMPILE);
-                                entry.setExported(true);
-                                doWriteAction(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        libraryModel.commit();
+                        public void run() {
+                            Module module = manager.newModule(pathToModule, JAVA_MODULE);
+                            String rootContent = path + Constants.SLASH + moduleName;
+                            String output = rootContent + Constants.CLASSES_DIR;
+                            ModuleRootModificationUtil.updateModel(module, new Consumer<ModifiableRootModel>() {
+                                @Override
+                                public void consume(ModifiableRootModel model) {
+                                    CompilerModuleExtension extension = model.getModuleExtension(CompilerModuleExtension.class);
+                                    ContentEntry rootContentEntry = model.addContentEntry(VfsUtilCore.pathToUrl(rootContent));
+                                    extension.inheritCompilerOutputPath(false);
+                                    extension.setCompilerOutputPath(VfsUtilCore.pathToUrl(output));
+                                    extension.setCompilerOutputPathForTests(VfsUtilCore.pathToUrl(output));
+                                    extension.setExcludeOutput(true);
+                                    model.inheritSdk();
+                                    boolean hasModuleInArtifactory = artifactory.containsKey(moduleName);
+                                    OrderEntry[] orderEntries = model.getOrderEntries();
+                                    NavigableSet<String> modulesDep = modulesDepUtil.getDepsForModule(moduleName);
+                                    for (OrderEntry entry : orderEntries) {
+                                        if ((entry instanceof ModuleOrderEntry) || (entry instanceof LibraryOrderEntry)) {
+                                            model.removeOrderEntry(entry);
+                                        }
                                     }
-                                });
-                            }
+                                    for (String moduleDep : modulesDep) {
+                                        model.addInvalidModuleEntry(moduleDep).setExported(true);
+                                    }
+                                    String pathToLib;
+                                    if (hasModuleInArtifactory && artifactoryDep) {
+                                        pathToLib = artifactory.get(moduleName);
+                                    } else {
+                                        try {
+                                            GeneratorSourceContent.generateRes(path, rootContentEntry, rootContent);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        pathToLib = path + Constants.SLASH + moduleName + Constants.SLASH + Constants.LIB_DIR;
+                                    }
+                                    File libs = new File(pathToLib);
+                                    if(libs.exists()) {
+                                        LibraryImpl library = (LibraryImpl) model.getModuleLibraryTable().createLibrary("Library");
+                                        LibraryEx.ModifiableModelEx libraryModel = library.getModifiableModel();
+                                        VirtualFile file = VfsUtilCore.findRelativeFile(pathToLib, project.getBaseDir());
+                                        libraryModel.addJarDirectory(file, true);
+                                        LibraryOrderEntry entry = model.findLibraryOrderEntry(library);
+                                        assert entry != null : library;
+                                        entry.setScope(DependencyScope.COMPILE);
+                                        entry.setExported(true);
+                                        doWriteAction(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                libraryModel.commit();
+                                            }
+                                        });
+                                    }
+                                }
+                            });
                         }
                     });
                 }
-            });
-        }
+            }
+        }, "Generating modules", false, project);
     }
 
     public void saveBaseModulesProperty(String baseModulesString, boolean artifactoryDep) {
